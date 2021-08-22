b8386f000d1f8c80012fca2d020f7f1f26fa3390
Zoubeiri <housna.zoubeiri@gmail.com>
2021-08-22 13:58:25 +0200

    HTML and CSS Header and Teddies Section

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ fetch('http://localhost:3000/api/teddies/')
   .then(function (responseData) {
    // Displaying Teddy One product
    let teddyOne = document.getElementById ('teddy-one');
-   teddyOne.innerHTML = '<h3>' + responseData[0].name +'</h3>'+'<img src="'+ responseData[0].imageUrl +'"alt= ></img>' + '<p>' + responseData[0].description +'</p>' +'<span>' + responseData[0].price + '</span>';
+   teddyOne.innerHTML = '<img src="'+ responseData[0].imageUrl +'"alt= ></img>' + '<h3>' + responseData[0].name +'</h3>'+ '<p>' + responseData[0].description +'</p>' +'<span>' + responseData[0].price + '</span>';
   
    let teddyTwo = document.getElementById ('teddy-two');
-   teddyTwo.innerHTML = '<h3>' + responseData[1].name +'</h3>'+'<img src="'+ responseData[1].imageUrl +'"alt= ></img>' + '<p>' + responseData[1].description +'</p>' +'<span>' + responseData[1].price + '</span>';
+   teddyTwo.innerHTML = '<img src="'+ responseData[1].imageUrl +'"alt= ></img>' + '<h3>' + responseData[1].name +'</h3>'+'<p>' + responseData[1].description +'</p>' +'<span>' + responseData[1].price + '</span>';
  
    let teddyThree = document.getElementById ('teddy-three');
-   teddyThree.innerHTML = '<h3>' + responseData[2].name +'</h3>'+'<img src="'+ responseData[2].imageUrl +'"alt= ></img>' + '<p>' + responseData[2].description +'</p>' +'<span>' + responseData[2].price + '</span>';
+   teddyThree.innerHTML = '<img src="'+ responseData[2].imageUrl +'"alt= ></img>' + '<h3>' + responseData[2].name +'</h3>'+'<p>' + responseData[2].description +'</p>' +'<span>' + responseData[2].price + '</span>';
   
    let teddyFour = document.getElementById ('teddy-four');
-   teddyFour.innerHTML = '<h3>' + responseData[3].name +'</h3>'+'<img src="'+ responseData[3].imageUrl +'"alt= ></img>' + '<p>' + responseData[3].description +'</p>' +'<span>' + responseData[3].price + '</span>';
+   teddyFour.innerHTML = '<img src="'+ responseData[3].imageUrl +'"alt= ></img>' + '<h3>' + responseData[3].name +'</h3>'+'<p>' + responseData[3].description +'</p>' +'<span>' + responseData[3].price + '</span>';
    
    let teddyFive = document.getElementById ('teddy-five');
-   teddyFive.innerHTML = '<h3>' + responseData[4].name +'</h3>'+'<img src="'+ responseData[4].imageUrl +'"alt= ></img>' + '<p>' + responseData[4].description +'</p>' +'<span>' + responseData[4].price + '</span>';
+   teddyFive.innerHTML = '<img src="'+ responseData[4].imageUrl +'"alt= ></img>' + '<h3>' + responseData[4].name +'</h3>'+'<p>' + responseData[4].description +'</p>' +'<span>' + responseData[4].price + '</span>';
   
   })
 
