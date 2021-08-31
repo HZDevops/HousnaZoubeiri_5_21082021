9eaa9e4278ffe7234a9240ca75ae09fc6dc15691
Zoubeiri <housna.zoubeiri@gmail.com>
2021-08-31 10:01:20 +0200

    JS code for checking validity of name, city and code zip values of the form

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,13 +1,13 @@
 // Getting Teddys' selected products in local Storage
-let teddyStockage = JSON.parse(localStorage.getItem("firstTeddy"));
-console.log(teddyStockage)
+let teddyStockage = JSON.parse(localStorage.getItem('firstTeddy'));
+console.log(teddyStockage);
 
 // Displaying Teddy's local Storage in shopping-cart page
-const teddyListStockage = document.getElementById('teddy-storage')
+const teddyListStockage = document.getElementById('teddy-storage');
 let teddyTotalPrice = [];
 
 teddyStockage.forEach(function (teddy) {
-    teddyListStockage.innerHTML += `
+  teddyListStockage.innerHTML += `
       <div class="teddy-ordered">
         <img src="${teddy.imageProduct}" alt="teddy selected by customer" />
         <div class="teddy-info">
@@ -17,26 +17,28 @@ teddyStockage.forEach(function (teddy) {
         </div>
         <span class="teddy-price">Prix: ${teddy.productPrice} €</span>
       </div>
-    `
-    teddyTotalPrice.push(`${teddy.productPrice}`)
-  })
+    `;
+  teddyTotalPrice.push(`${teddy.productPrice}`);
+});
 //console.log(teddyTotalPrice)
 
 //Converting an array of strings(teddyTotalPrice) to an array of numbers
 const teddyTotalPriceNumber = teddyTotalPrice.map((i) => Number(i));
-console.log(teddyTotalPriceNumber)
+console.log(teddyTotalPriceNumber);
 
-let shoppingCartTotalPrice = 0
+let shoppingCartTotalPrice = 0;
 //Calculating and displaying total price of shopping-cart
-for (let i = 0; i <teddyTotalPriceNumber.length; i++) {
+for (let i = 0; i < teddyTotalPriceNumber.length; i++) {
   shoppingCartTotalPrice += teddyTotalPriceNumber[i];
 }
 console.log(shoppingCartTotalPrice);
-document.getElementById("shopping-cart-amount").innerHTML =`<p>Montant total : ${shoppingCartTotalPrice}€</p>`
+document.getElementById(
+  'shopping-cart-amount'
+).innerHTML = `<p>Montant total : ${shoppingCartTotalPrice}€</p>`;
 
 // Code for shopping-cart form
 
-const teddyFormHtml = document.getElementById("teddy-form")
+const teddyFormHtml = document.getElementById('teddy-form');
 
 teddyFormHtml.innerHTML = `
 <p>Remplir le formulaire pour valider la commande</p>
@@ -62,33 +64,69 @@ teddyFormHtml.innerHTML = `
         <input type="customer-email" id="customer-email" name="customer-email" placeholder="Ex: monadresse@mail.com" required>
     </div>
 </form>
-<button id="send-form-btn" type="submit" name="send-btn">Confirmer la commande</button>`
+<button id="send-form-btn" type="submit" name="send-btn">Confirmer la commande</button>`;
 
 // Sending shopping-cart form in stockage array before displaying in shopping-cart
-const sendButtonForm = document.getElementById("send-form-btn")
+const sendButtonForm = document.getElementById('send-form-btn');
 //console.log(sendButtonForm)
 
-sendButtonForm.addEventListener('click', function(event) {
-  event.preventDefault()
+sendButtonForm.addEventListener('click', function (event) {
+  event.preventDefault();
 
   //Getting form values in an object
 
-  const formValues ={
-    name: document.getElementById("customer-name").value,
-    adress: document.getElementById("customer-address").value,
-    city: document.getElementById("customer-city").value,
-    codeZip: document.getElementById("code-zip").value,
-    email: document.getElementById("customer-email").value
+  const formValues = {
+    name: document.getElementById('customer-name').value,
+    address: document.getElementById('customer-address').value,
+    city: document.getElementById('customer-city').value,
+    codeZip: document.getElementById('code-zip').value,
+    email: document.getElementById('customer-email').value,
+  };
+
+  //*******Checking validity form values******//
+
+  //Checking validity name value
+  function validityName() {
+    const nameToCheck = formValues.name;
+    if (/^[A-Za-z]{3,30}$/.test(nameToCheck)) {
+      return true;
+    } else {
+      alert('Chiffres et symboles non autorisés \n Saisir 3 à 30 caractères');
+      return false;
+    }
+  }
+  //Checking city value
+  function validityCity() {
+    const cityToCheck = formValues.city;
+    if (/^[A-Za-z]{3,20}$/.test(cityToCheck)) {
+      return true;
+    } else {
+      alert('Chiffres et symboles non autorisés \n Saisir 3 à 20 caractères');
+      return false;
+    }
+  }
+  //Checking code zip value
+  function validityCodeZip() {
+    const codeZipToCheck = formValues.codeZip;
+    if (/^[0-9]{5}$/.test(codeZipToCheck)) {
+      return true;
+    } else {
+      alert('Lettres et symboles non autorisés \n Saisir 5 chiffres');
+      return false;
+    }
   }
 
-//Putting form values in local Storage
-localStorage.setItem("formValues",JSON.stringify(formValues))
-    //console.log(document.getElementById("customer-name").value)
-   
-//Putting form and shopping-cart products in a object before sending to server
-const sendShoppingCartToServer = {
-  teddyStockage,
-  formValues
-}
-//console.log(sendShoppingCartToServer);
-  })
+  //Checking validity form values before putting them in local Storage
+  if (validityName() && validityCity() && validityCodeZip()) {
+    localStorage.setItem('formValues', JSON.stringify(formValues));
+  } else {
+    alert('Veuillez bien remplir le formulaire');
+  }
+
+  //Putting form and shopping-cart products in a object before sending to server
+  const sendShoppingCartToServer = {
+    teddyStockage,
+    formValues,
+  };
+  //console.log(sendShoppingCartToServer);
+});
