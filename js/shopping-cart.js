b2bb4a4d358fbc3b10cf7baddf7261a5aaa7053d
Zoubeiri <housna.zoubeiri@gmail.com>
2021-09-12 17:04:39 +0200

    Adding responsive

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,15 +1,20 @@
 // Get item selected products in local Storage
 const shoppingCart = getFromLocalStorage('orinoco-shopping-cart');
 
-// Display item from localStorage to shopping-cart page
+// Display items on shopping-cart page if items in localStorage
 const itemListInHtml = document.getElementById('teddy-shopping-cart');
 const itemTotalPrice = [];
 const itemQuantity = [];
 const products = [];
-if (shoppingCart) {
+
+if (!shoppingCart) {
+  const emptyCart = document.createElement('p');
+  itemListInHtml.appendChild(emptyCart);
+  emptyCart.textContent = 'Votre panier est vide !';
+} else {
   shoppingCart.forEach(function (item) {
     itemListInHtml.innerHTML += `
-      <div class="teddy-ordered">
+      <div class="teddy-cart">
         <img src="${item.imageUrl}" alt="teddy selected by customer" />
         <div class="teddy-info">
           <h3>${item.name}</h3>
@@ -17,28 +22,30 @@ if (shoppingCart) {
           <span>Quantité: ${item.quantity}</span>
         </div>
         <span class="teddy-price">Prix: ${item.price / 100}€</span>
+        <button class="garbage-button" title="Supprimer cet article?"><i class="fas fa-trash-alt"></i></button>
       </div>
     `;
     products.push(item._id);
     itemTotalPrice.push(item.price / 100);
     itemQuantity.push(item.quantity);
   });
-
-  //Calculate and display the amount of the shopping-cart
-  let shoppingCartAmount = 0;
-  for (let i = 0; i < itemTotalPrice.length; i++) {
-    shoppingCartAmount += itemTotalPrice[i] * itemQuantity[i];
-  }
-  document.getElementById(
-    'shopping-cart-amount'
-  ).textContent = `Montant total : ${shoppingCartAmount}€`;
-  saveToLocalStorage('shopping-cart-amount', shoppingCartAmount);
-
 }
+//Calculate shopping-cart amount
+const shoppingCartAmount = calculateShoppingCartAmount(
+  itemTotalPrice,
+  itemQuantity
+);
 
+//Display the shopping-cart amount on html and save it in localStorage
+const cartAmount = document.createElement('span');
+itemListInHtml.appendChild(cartAmount);
+cartAmount.id = 'shopping-cart-amount';
+cartAmount.textContent = `Montant total : ${shoppingCartAmount}€`;
+saveToLocalStorage('shopping-cart-amount', shoppingCartAmount);
+
+//Put form values in object
 const form = document.getElementById('contact-form');
 form.addEventListener('submit', function (e) {
-  //Put form values in object
   const contact = {
     firstName: document.getElementById('customer-first-name').value,
     lastName: document.getElementById('customer-last-name').value,
--- js/utils.js
+++ js/utils.js
@@ -2,26 +2,40 @@
  * Get Id product from URL query string
  * @returns {string} productId
  */
-function getProductIdFromUrl(){ 
-const queryStringUrlId = window.location.search
- const urlSearchParams = new URLSearchParams(queryStringUrlId)
- const productId = urlSearchParams.get('id')
- return productId;
+function getProductIdFromUrl() {
+  const queryStringUrlId = window.location.search;
+  const urlSearchParams = new URLSearchParams(queryStringUrlId);
+  const productId = urlSearchParams.get('id');
+  return productId;
 }
 /**
  * Save an item to local storage
  * @param {string} key
  * @param {Array} value
  **/
-function saveToLocalStorage (key, value) {
+function saveToLocalStorage(key, value) {
   localStorage.setItem(key, JSON.stringify(value));
 }
 
 /**
  * Get an item from local storage
- * @param {string} key 
+ * @param {string} key
  * @returns {Array}
  */
-function getFromLocalStorage (key) {
+function getFromLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
+}
+
+/**
+ * Calculate the total price of a shopping-cart
+ * @param {Array} price 
+ * @param {Array} quantity 
+ * @returns {number}
+ */
+function calculateShoppingCartAmount(price, quantity) {
+  let totalPrice = 0;
+  for (let i = 0; i < price.length; i++) {
+    totalPrice += price[i] * quantity[i];
+  }
+  return totalPrice;
 }
