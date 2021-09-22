b98c5566ce462c196dfa631f30a9b3aff0d6adf0
Zoubeiri <housna.zoubeiri@gmail.com>
2021-09-22 13:31:27 +0200

    cleaning

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -97,13 +97,11 @@ function sendShoppinCartToServer() {
           })
           .then(function (data) {
             const orderConfirmation = data;
-            //localStorage.setItem('orinoco-order-info', orderConfirmation.orderId);
             window.location =
               'order-confirmation.html?orderid=' +
               `${orderConfirmation.orderId}` +
               '&montant=' +
               `${shoppingCartAmount}`;
-            //localStorage.removeItem('orinoco-shopping-cart');
           })
           .catch(function (error) {
             document.getElementById('error-message').innerText =
